ae99fbff1a495fc8d1c341f82f287245a3549303
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-17 12:49:56 -0700

    Takes args from command line and can make markov chains from n-grams of any size

--- markov.py
+++ markov.py
@@ -1,6 +1,7 @@
 """Generate Markov text from text files."""
 
 from random import choice
+from sys import argv
 
 def open_and_read_file(file_path):
     """Take file path as string; return text as string.
@@ -12,7 +13,7 @@ def open_and_read_file(file_path):
     with open(file_path) as data_file:
         return data_file.read()
 
-def make_chains(text_string):
+def make_bigram_chains(text_string):
     """Take input text as string; return dictionary of Markov chains.
 
     A chain will be a key that consists of a tuple of (word1, word2)
@@ -65,8 +66,31 @@ def make_chains(text_string):
     #print(chains)
     return chains
 
+def make_chains_any_size(text_string, size):
 
-def make_text(chains):
+    split_string = text_string.split()
+
+    chains = {}
+
+    for i in range(0, (len(split_string)-size)):
+        ngram_word_list = []                            #empty list to work on
+
+        for j in range(0, size):                        #increments how many words that compose the tuple
+            ngram_word_list.append(split_string[i+j])   #adds all the words from 0 to n to a list
+            ngram_tuple = tuple(ngram_word_list)        #makes the entire list a tuple
+
+        word_after = split_string[i+size]               #saves the word after the n amount of words
+
+        if ngram_tuple not in chains:       
+            chains[ngram_tuple] = [word_after]
+
+        else:
+            chains[ngram_tuple].append(word_after)
+
+    #print(chains)
+    return chains
+
+def make_bigram_text(chains):
     """Return text from chains."""
 
     words = []
@@ -83,7 +107,7 @@ def make_text(chains):
             words.append(markov_word)
             # print("current word list is " + str(words))
 
-            generative_key = (generative_key[1], markov_word)
+            generative_key = (generative_key[-1], markov_word)
             # print("next generative key is " + str(generative_key))
 
         except KeyError:
@@ -92,16 +116,41 @@ def make_text(chains):
 
     return " ".join(words)
 
+def make_ngram_text(chains):
 
-input_path = "raven.txt"
+    words = []
+
+    generative_key = choice(list(chains.keys()))
+    #print("key initialized as " + str(generative_key))
+
+    while True:
+
+        try:
+            markov_word = choice(chains[generative_key])
+            words.append(markov_word)
+
+            generative_key_as_list = list(generative_key) #converts generative key(tuple) into list
+            generative_key = tuple(generative_key_as_list[1:] + [markov_word]) #adds markov word to the list without first word and coverts into tuple
+        
+        except KeyError:
+            break 
+
+    return " ".join(words)
+
+
+input_path = argv[1]
+
+ngram_size = int(argv[2])
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text)
+# chains = make_bigram_chains(input_text)
+chains = make_chains_any_size(input_text, ngram_size)
 
 # Produce random text
-random_text = make_text(chains)
+#random_text = make_bigram_text(chains)
+random_text = make_ngram_text(chains)
 
 print(random_text)
